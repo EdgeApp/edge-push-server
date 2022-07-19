c4018d2040f71dd7a53a9468c4f0c99acaa1429d
Xipu Li <lixipu98@gmail.com>
2022-07-19 15:13:25 -0700

    fixup! Define task related types and reorg types folder

--- src/types/task/ActionEffect.ts
+++ src/types/task/ActionEffect.ts
@@ -1,5 +1,4 @@
 import {
-  asArray,
   asBoolean,
   asEither,
   asNumber,
@@ -13,98 +12,71 @@ import {
 // -------------------------------------------------------------------
 // Type definitions
 // -------------------------------------------------------------------
-
-export interface GeneralActionEffect {
-  completed: boolean
-}
-export interface SeqActionEffect extends GeneralActionEffect {
-  type: 'seq'
-  opIndex: number
-  childEffect: ActionEffect
-}
-
-export interface ParActionEffect extends GeneralActionEffect {
-  type: 'par'
-  childEffects: ActionEffect[]
-}
-
-export interface BalanceActionEffect extends GeneralActionEffect {
-  type: 'balance'
-  address: string
-  aboveAmount?: string
-  belowAmount?: string
-  walletId: string
-  tokenId?: string
-}
-
-export interface TxConfsActionEffect extends GeneralActionEffect {
-  type: 'tx-confs'
-  txId: string
-  walletId: string
-  confirmations: number
-}
-
-export interface PriceActionEffect extends GeneralActionEffect {
-  type: 'price'
-  currencyPair: string
-  aboveRate?: string
-  belowRate?: string
-}
-// TODO: @samholmes to add comments
 export type ActionEffect =
-  | SeqActionEffect
-  | ParActionEffect
-  | BalanceActionEffect
-  | TxConfsActionEffect
-  | PriceActionEffect
+  | {
+      type: 'balance'
+      completed: boolean
+      params: {
+        address: string
+        aboveAmount?: string
+        belowAmount?: string
+        contractAddress?: string
+        network: string
+      }
+    }
+  | {
+      type: 'tx-confs'
+      completed: boolean
+      params: {
+        confirmations: number
+        txId: string
+        network: string
+      }
+    }
+  | {
+      type: 'price'
+      completed: boolean
+      params: {
+        aboveRate?: string
+        belowRate?: string
+        currencyPair: string
+        network: string
+      }
+    }
 
 // -------------------------------------------------------------------
 // Cleaners definitions
 // -------------------------------------------------------------------
 
-export const asSeqActionEffect: Cleaner<SeqActionEffect> = asObject({
-  type: asValue('seq'),
-  opIndex: asNumber,
-  completed: asBoolean,
-  childEffect: raw => asActionEffect(raw)
-})
-
-export const asParActionEffect: Cleaner<ParActionEffect> = asObject({
-  type: asValue('par'),
-  completed: asBoolean,
-  childEffects: asArray(raw => asActionEffect(raw))
-})
-
-export const asBalanceActionEffect: Cleaner<BalanceActionEffect> = asObject({
-  type: asValue('balance'),
-  address: asString,
-  completed: asBoolean,
-  aboveAmount: asOptional(asString),
-  belowAmount: asOptional(asString),
-  walletId: asString,
-  tokenId: asOptional(asString)
-})
-
-export const asTxConfsActionEffect: Cleaner<TxConfsActionEffect> = asObject({
-  type: asValue('tx-confs'),
-  txId: asString,
-  completed: asBoolean,
-  walletId: asString,
-  confirmations: asNumber
-})
-
-export const asPriceActionEffect: Cleaner<PriceActionEffect> = asObject({
-  type: asValue('price'),
-  currencyPair: asString,
-  completed: asBoolean,
-  aboveRate: asOptional(asString),
-  belowRate: asOptional(asString)
-})
-
 export const asActionEffect: Cleaner<ActionEffect> = asEither(
-  asSeqActionEffect,
-  asParActionEffect,
-  asBalanceActionEffect,
-  asTxConfsActionEffect,
-  asPriceActionEffect
+  asObject({
+    type: asValue('balance'),
+    completed: asBoolean,
+    params: asObject({
+      address: asString,
+      aboveAmount: asOptional(asString),
+      belowAmount: asOptional(asString),
+      contractAddress: asOptional(asString),
+      network: asString
+    })
+  }),
+  asObject({
+    type: asValue('tx-confs'),
+    completed: asBoolean,
+    params: asObject({
+      confirmations: asNumber,
+      txId: asString,
+      network: asString
+    })
+  }),
+  asObject({
+    type: asValue('price'),
+    completed: asBoolean,
+    params: asObject({
+      aboveRate: asOptional(asString),
+      belowRate: asOptional(asString),
+      currencyPair: asString,
+      network: asString
+    })
+  })
 )
