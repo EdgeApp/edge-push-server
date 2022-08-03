4cc7aaa969eb2e6dd181ab9c0efeda7c3f3e19a8
William Swanson <swansontec@gmail.com>
2022-08-02 22:39:27 -0700

    Clean up serverlet request types

--- src/types/requestTypes.ts
+++ src/types/requestTypes.ts
@@ -11,10 +11,9 @@ export interface Logger {
 }
 
 export interface LoggedRequest extends ExpressRequest {
-  // Logging stuff:
-  date: Date
-  ip: string
-  log: Logger
+  readonly date: Date
+  readonly ip: string
+  readonly log: Logger
 }
 
 export interface DbRequest extends LoggedRequest {
@@ -25,6 +24,6 @@ export interface ApiRequest extends DbRequest {
   readonly apiKey: ApiKey
 
   // Taken from the Express request:
-  json: unknown
-  query: unknown
+  readonly json: unknown
+  readonly query: unknown
 }
