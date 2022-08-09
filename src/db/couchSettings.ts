4c06c9cbe92ab6c187d8ceab0c03e097b4fed0ad
William Swanson <swansontec@gmail.com>
2022-08-09 16:28:58 -0700

    Remove the legacy price script

--- src/db/couchSettings.ts
+++ src/db/couchSettings.ts
@@ -1,11 +1,4 @@
-import {
-  asArray,
-  asBoolean,
-  asMaybe,
-  asNumber,
-  asObject,
-  asString
-} from 'cleaners'
+import { asArray, asBoolean, asMaybe, asObject, asString } from 'cleaners'
 import {
   asReplicatorSetupDocument,
   DatabaseSetup,
@@ -26,13 +19,8 @@ const asSettings = asObject({
     []
   ),
 
-  // Default thresholds:
-  defaultAnomaly: asMaybe(asNumber, 90),
-  defaultHours: asMaybe(asObject(asNumber), { '1': 3, '24': 10 }),
-
   // Mode toggles:
   debugLogs: asMaybe(asBoolean, false),
-  priceCheckInMinutes: asMaybe(asNumber, 5),
 
   // Other services we rely on:
   slackUri: asMaybe(asString, '')
