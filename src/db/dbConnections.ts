741bb0cdc11e51343a00638ce2bbf4cfa6519690
William Swanson <swansontec@gmail.com>
2024-12-31 14:57:52 -0800

    Close the AMQP client on CLI exit

--- src/db/dbConnections.ts
+++ src/db/dbConnections.ts
@@ -1,7 +1,8 @@
-import { AMQPQueue } from '@cloudamqp/amqp-client'
+import { AMQPClient, AMQPQueue } from '@cloudamqp/amqp-client'
 import { ServerScope } from 'nano'
 
 export interface DbConnections {
   readonly couch: ServerScope
+  readonly amqpClient: AMQPClient
   readonly queue: AMQPQueue
 }
